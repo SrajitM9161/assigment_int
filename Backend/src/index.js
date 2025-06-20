849cd2f005a37ca27d33507c79d4d95a775ef237
Srajit Mishra <neetuuno123@gmail.com>
2025-06-20 23:56:08 +0530

    final deployment version 13

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -7,13 +7,13 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(cors({
-  origin: "https://assigment-int.vercel.app/",  // ✅ Replace with actual domain
+  origin: "https://assigment-int.vercel.app",  // ✅ Replace with actual domain
   credentials: true,
 }));
 
 const io = socketio(server, {
   cors: {
-    origin: "https://assigment-int.vercel.app/",  // ✅ Replace with actual domain
+    origin: "https://assigment-int.vercel.app",  // ✅ Replace with actual domain
     methods: ["GET", "POST"],
     credentials: true,
   },
